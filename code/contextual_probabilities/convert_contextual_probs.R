9bf90730ee635b7500fae3f63c0b1305830a4e19
Molly Offer-Westort <mollyow@gmail.com>
2023-03-21 19:33:29 -0500

    more tidying

--- code/contextual_probabilities/convert_contextual_probs.R
+++ code/contextual_probabilities/convert_contextual_probs.R
@@ -1,7 +1,9 @@
 
-library(reticulate)
-use_python('/opt/anaconda3/bin') # set appropriate python
 
-np <- import("numpy")
-probs_array <- np$load("../../data/contextual_probabilities.npy")
+library(reticulate)
+# install_miniconda() may need to install miniconda, or otherwise set python version
+options(reticulate.conda_binary = miniconda_path())
+
+np <- import('numpy')
+probs_array <- np$load('../../data/contextual_probabilities.npy')
 saveRDS(probs_array, '../objects/contextual_probabilities.RDS')
